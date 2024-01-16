52479f314ff44a1b64318a20e6646d4795e2d23c
ShofiqulMiraz <simiraz90@gmail.com>
2024-01-17 00:00:55 +0600

    some update

--- src/pages/allscammerpage/allscammerpage.js
+++ src/pages/allscammerpage/allscammerpage.js
@@ -9,64 +9,62 @@ import { useHistory } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 
 const AllScammerPage = () => {
-  let history = useHistory();
-  const [scamssortedbylikes, setscamssortedbylikes] = useState([]);
-  const [loading, setloading] = useState(false);
-  const [searchterm, setsearchterm] = useState("");
+    let history = useHistory();
+    const [scamssortedbylikes, setscamssortedbylikes] = useState([]);
+    const [loading, setloading] = useState(false);
+    const [searchterm, setsearchterm] = useState("");
 
-  const getScamsByLikes = async () => {
-    try {
-      setloading(true);
-      const response = await axios.get(`/scams/?limit=20`);
-      const scams = response.data;
-      setscamssortedbylikes(scams);
-      setloading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getScamsByLikes();
-  }, []);
+    const getScamsByLikes = async () => {
+        try {
+            setloading(true);
+            const response = await axios.get(`/scams/?limit=20`);
+            const scams = response.data;
+            setscamssortedbylikes(scams);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        getScamsByLikes();
+    }, []);
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    history.push(`/search/${searchterm}`);
-  };
-  return (
-    <>
-      <Helmet>
-        <title>CryptoScamAlert | All Scammers</title>
-      </Helmet>
-      <section className="allscammerpage mt-2">
-        <div className="container">
-          <div className="row">
-            <div className="allscammerpage__head">
-              <h1 className="allscammerpage__head-title">
-                All Crypto Scammers (1036)
-              </h1>
-              <p className="allscammerpage__head-para">
-                Below are the list of scammers in the CryptoScamAlert database.
-                Scammers list ordered by voting.
-              </p>
-            </div>
-            <form className="search__input mb-4" onSubmit={handleSearch}>
-              <input
-                type="text"
-                name="search"
-                placeholder="Type domain,resource or company name and press Enter"
-                onChange={(e) => setsearchterm(e.target.value)}
-              />
-            </form>
-            {loading && skeletonUIForDetailsPage()}
-            <Scam scams={scamssortedbylikes} />
-          </div>
-        </div>
-        <ScamGallery />
-        <Footer />
-      </section>
-    </>
-  );
+    const handleSearch = (e) => {
+        e.preventDefault();
+        history.push(`/search/${searchterm}`);
+    };
+    return (
+        <>
+            <Helmet>
+                <title>CryptoScamAlert | All Scammers</title>
+            </Helmet>
+            <section className="allscammerpage mt-2">
+                <div className="container">
+                    <div className="row">
+                        <div className="allscammerpage__head">
+                            <h1 className="allscammerpage__head-title">All Crypto Scammers (1036)</h1>
+                            <p className="allscammerpage__head-para">
+                                Below are the list of scammers in the CryptoScamAlert database. Scammers list ordered by voting.
+                            </p>
+                        </div>
+                        <form className="search__input mb-4" onSubmit={handleSearch}>
+                            <input
+                                type="text"
+                                name="search"
+                                placeholder="Type domain,resource or company name and press Enter"
+                                onChange={(e) => setsearchterm(e.target.value)}
+                            />
+                        </form>
+                        {loading && skeletonUIForDetailsPage()}
+                        <Scam scams={scamssortedbylikes} />
+                        {loading && <div className="pt-4"></div>}
+                    </div>
+                </div>
+                <ScamGallery />
+                <Footer />
+            </section>
+        </>
+    );
 };
 
 export default AllScammerPage;
--- src/pages/forgotpassword/forgotpassword.js
+++ src/pages/forgotpassword/forgotpassword.js
@@ -1,69 +1,67 @@
-import "./forgotpassword.scss";
+import axios from "axios";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import "./forgotpassword.scss";
 
 const ForgotPassword = () => {
-  const { isloggedin } = useSelector((state) => state.userReducer);
-  const { register, handleSubmit } = useForm();
-  const [loading, setloading] = useState(false);
-  const onSubmit = async (data) => {
-    try {
-      setloading(true);
-      const res = await axios.post("/users/forgotpassword", data, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      setloading(false);
-      toast.success(res.data);
-    } catch (error) {
-      const err = error.response.data;
-      toast.error(err);
-      setloading(false);
+    const { isloggedin } = useSelector((state) => state.userReducer);
+    const { register, handleSubmit } = useForm();
+    const [loading, setLoading] = useState(false);
+    const onSubmit = async (data) => {
+        try {
+            setLoading(true);
+            const res = await axios.post("/users/forgotpassword", data, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            setLoading(false);
+            toast.success(res.data);
+        } catch (error) {
+            const err = error.response.data;
+            toast.error(err);
+            setLoading(false);
+        }
+    };
+
+    if (isloggedin) {
+        return <Redirect to="/" />;
     }
-  };
 
-  if (isloggedin) {
-    return <Redirect to="/" />;
-  }
-
-  return (
-    <>
-      <Helmet>
-        <title>CryptoScamAlert | Forgot Password</title>
-      </Helmet>
-      <section className="forgotpassword">
-        <div className="container forgotpassword__container">
-          <div className="row">
-            <h3 className="forgotpassword__head pt-5">
-              forgot your password? Let us help!
-            </h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-floating mb-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  placeholder="name@example.com"
-                  required
-                  ref={register}
-                />
-                <label htmlFor="email">Enter Your Email address</label>
-              </div>
-              <button type="submit" className="btn btn-primary">
-                {loading ? "Sending Reset token..." : "Get Reset Token"}
-              </button>
-            </form>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>CryptoScamAlert | Forgot Password</title>
+            </Helmet>
+            <section className="forgotpassword">
+                <div className="container forgotpassword__container">
+                    <div className="row">
+                        <h3 className="forgotpassword__head pt-5">Forgot your password? Let us help!</h3>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    name="email"
+                                    placeholder="name@example.com"
+                                    required
+                                    ref={register}
+                                />
+                                <label htmlFor="email">Enter Your Email address</label>
+                            </div>
+                            <button type="submit" className="btn btn-primary">
+                                {loading ? "Sending Reset token..." : "Get Reset Token"}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default ForgotPassword;
